3d89d090f873af6081d13d9d705cdaf1a556f710
David Urting <david.urting@live.com>
2018-12-22 19:53:48 +0100

    DropDownDemo

--- Demo.Web/Controllers/BindingController.cs
+++ Demo.Web/Controllers/BindingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Demo.Models.Binding;
+using Demo.Web.Models.Binding;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -14,7 +15,7 @@ namespace Demo.Controllers
         [HttpGet]
         public IActionResult MultipleCheckboxViaFormPost()
         {
-            var model = CreateViewModelWithDummyData();
+            var model = CreateMultipleCheckboxViewModelWithDummyData();
             return View(model);
         }
 
@@ -27,7 +28,7 @@ namespace Demo.Controllers
 
         public IActionResult MultipleCheckboxViaAjaxPost()
         {
-            var model = CreateViewModelWithDummyData();
+            var model = CreateMultipleCheckboxViewModelWithDummyData();
             return View(model);
         }
 
@@ -38,7 +39,7 @@ namespace Demo.Controllers
             return Json($"Jij vinkte {string.Join(',', aangvinkteIds)} aan");
         }
 
-        private MultipleCheckboxViewModel CreateViewModelWithDummyData()
+        private MultipleCheckboxViewModel CreateMultipleCheckboxViewModelWithDummyData()
         {
             MultipleCheckboxViewModel model = new MultipleCheckboxViewModel();
             foreach (var id in new string[] { "A", "B", "C", "D", "E" })
@@ -50,6 +51,50 @@ namespace Demo.Controllers
                     Text = $"Hier wat uitleg over {id}"
                 });
             }
+            return model;
+        }
+        #endregion
+
+        #region DropDown
+        public IActionResult DropDownViaFormPost()
+        {
+            DropDownViewModel model = CreateDropDownViewModelWithDummyData();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult DropDownViaFormPost(DropDownViewModel model)
+        {
+            return Json($"Jij selecteerde de value '{model.SelectedValue}'.");
+        }
+
+        private DropDownViewModel CreateDropDownViewModelWithDummyData()
+        {
+            DropDownViewModel model = new DropDownViewModel();
+            model.DropDownItems.Add(new SelectListItem()
+            {
+                Value = "value1",
+                Text = "Value One",
+                Selected = false
+            });
+            model.DropDownItems.Add(new SelectListItem()
+            {
+                Value = "value2",
+                Text = "Value Two",
+                Selected = false
+            });
+            model.DropDownItems.Add(new SelectListItem()
+            {
+                Value = "value3",
+                Text = "Value Three",
+                Selected = true // <- Deze wordt 'gepreselecteerd'.
+            });
+            model.DropDownItems.Add(new SelectListItem()
+            {
+                Value = "value4",
+                Text = "Value Four",
+                Selected = false
+            });
             return model;
         }
         #endregion
---
+++ Demo.Web/Models/Binding/DropDownViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Web.Models.Binding
+{
+    public class DropDownViewModel
+    {
+        public List<SelectListItem> DropDownItems { get; set; } = new List<SelectListItem>();
+        public string SelectedValue { get; set; }
+    }
+}
